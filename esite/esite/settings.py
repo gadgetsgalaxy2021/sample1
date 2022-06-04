ce9d35595907f8da6e26f12bce82b85ad21805a4
Priyansh <gadgetsgalaxy2021@gmail.com>
2022-06-04 09:53:15 +0530

    ok

--- esite/esite/settings.py
+++ esite/esite/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,7 @@ PASSWORD_HASHERS = [
 #     BASE_DIR / "static",
 #     '/var/www/static/',
 # ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/images/'
